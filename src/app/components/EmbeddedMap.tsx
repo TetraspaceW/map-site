aa0b2855f46c323c188ba36aa00450ee1722f6f5
TetraspaceW <holomanga@gmail.com>
2023-12-03 01:52:47 +0000

    abstract airports graph

--- src/app/components/EmbeddedMap.tsx
+++ src/app/components/EmbeddedMap.tsx
@@ -1,37 +1,80 @@
 import styles from "./styles.module.css";
 
-import { useMemo } from "react";
-import { Pin } from "../types/MapTypes";
+import { Node, Airport, Route } from "../types/MapTypes";
 import { LoadingSkeleton } from "./LoadingSkeleton";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { ForceGraph2D } from "react-force-graph";
 
 export const EmbeddedMap = ({
-  locations,
-  locationsLoaded = true,
+  nodes,
+  airports,
+  routes,
+  loaded = true,
 }: {
-  locations: Pin[];
-  locationsLoaded: boolean;
+  nodes: Node[];
+  airports: Airport[];
+  routes: Route[];
+  loaded: boolean;
 }) => {
-  return locationsLoaded ? (
-    <MapComponent locations={locations} />
+  return loaded ? (
+    <MapComponent nodes={nodes} airports={airports} routes={routes}/>
   ) : (
     <LoadingSkeleton />
   );
 };
 
-const MapComponent = ({ locations }: { locations: Pin[] }) => {
-  const center = useMemo(() => ({ lat: 0, lng: 0 }), []);
+const MapComponent = ({
+  nodes,
+  airports,
+  routes
+}: {
+  nodes: Node[];
+  airports: Airport[];
+  routes: Route[]
+}) => {
+  const graphLinks = nodes.map((node) => {
+    return { source: node.user_id, target: node.nearest_airport };
+  }).concat(routes.map((route) => {
+    return { source: route.start, target: route.end }
+  }));
+
+  const graphNodes = nodes
+    .map((node) => {
+      return {
+        id: node.user_id,
+        name: node.user_name,
+        color: "#aa00aa",
+        type: "node",
+        val: 1,
+      };
+    })
+    .concat(
+      airports.map((airport) => {
+        return {
+          id: airport.code,
+          name: airport.code,
+          val: 4,
+          color: "#888888",
+          type: "airport",
+        };
+      })
+    );
 
   return (
-    <APIProvider apiKey={process.env.GOOGLE_MAPS_KEY ?? ""}>
-      <Map zoom={1} center={center} mapContainerClassName={styles.map}>
-        {locations.map((pin) => {
-          const { location, user_name } = pin;
-          return (
-            <Marker key={user_name} position={location} label={user_name} />
-          );
-        })}
-      </Map>
-    </APIProvider>
+    <ForceGraph2D
+      graphData={{
+        nodes: graphNodes,
+        links: graphLinks,
+      }}
+      nodeCanvasObject={(node, ctx, globalScale) => {
+        if (node.x && node.y) {
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle";
+          ctx.font = `${node.val * 4}px Sans-Serif`;
+          ctx.fillStyle = "black";
+          ctx.fillText(node.name, node.x, node.y);
+        }
+      }}
+      nodeCanvasObjectMode={() => "after"}
+    />
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,37 @@ import { AppError } from "./types/ErrorTypes";
 const fetcher = (...args: any) => fetch(args).then((res) => res.json());
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR("/api/locations", fetcher);
-  const locations = data?.locations ?? [];
+  const {
+    data: locationData,
+    error: locationError,
+    isLoading: locationIsLoading,
+  } = useSWR("/api/locations", fetcher);
+  const {
+    data: airportData,
+    error: airportError,
+    isLoading: airportIsLoading,
+  } = useSWR("api/airports", fetcher);
+  const {
+    data: routeData,
+    error: routeError,
+    isLoading: routeIsLoading,
+  } = useSWR("api/airports", fetcher);
+  // TODO: this is starting to get a bit embarrasing, why is it repeated thrice
+  const nodes = locationData?.locations ?? [];
+  const airports = airportData?.airports ?? [];
+  const routes = routeData?.routes ?? [];
 
   return (
     <main className={styles.main}>
-      {error ? (
+      {locationError || airportError || routeError ? (
         <ErrorMessage message={AppError.LocationFetchError} />
       ) : (
-        <EmbeddedMap locations={locations} locationsLoaded={!isLoading} />
+        <EmbeddedMap
+          nodes={nodes}
+          airports={airports}
+          routes={routes}
+          loaded={!(airportIsLoading || locationIsLoading || routeIsLoading)}
+        />
       )}
     </main>
   );
